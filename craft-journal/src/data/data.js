391f0dba392f4e5a941617c132456b9c16de4788
klytne <tienganekelly@gmail.com>
2025-07-15 01:35:34 +0200

    Create data file

---
+++ craft-journal/src/data/data.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        title: "Beautiful Planet Pillow",
+        location: "Home",
+        links: {
+            Instagram: "https://www.instagram.com/p/DHINknetd5j/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+            Yaga: "https://www.yaga.co.za/littlecrochettime/product/bhtibnnqie"
+        },
+        description: "A tapestry pillow with a planet with the LGBTI+ flag colors on it's face. The back completes the face with stripes of the same colors. White is used as the background to make the tapestry stand out even more.",
+        images: {
+            1: "craft-journal/src/assets/20250227_093326.jpg",
+            2: "craft-journal/src/assets/20250227_093619.jpg"
+        },
+        hours: "",
+    },
+    {
+        title: "Megumi Fushiguro's Toad Pillow",
+        location: "Home",
+        links: {
+            Instagram: "https://www.instagram.com/p/DKM6ZdSIzF_/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+            Yaga: "https://www.yaga.co.za/littlecrochettime/product/vi6qvfr1eo"
+        },
+        description: "Inspired by the iconic shikigami from Jujutsu Kaisen, the face shows Megumi Fushiguro's hand sign which summons the Toad, a shikigami. Behind his hands is the shikigami. The back is just white with no tapestry.",
+        images: {
+            1: "craft-journal/src/assets/20250528_080734.jpg",
+            2: "craft-journal/src/assets/20250528_080750.jpg"
+        },
+        hours: "",
+    },
+    {
+        title: "Sunflower Pillow",
+        location: "Home",
+        links: {
+            Instagram: "https://www.instagram.com/p/DFnk6BhNQQJ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+            Yaga: "#"
+        },
+        description: "A simple tapestry of a sunflower on the face. Black is used as the backgrund to make the sunflower pop out more.",
+        images: {
+            1: "craft-journal/src/assets/20250203_163744.jpg",
+            2: "#"
+        },
+        hours: "",
+    }
+]
