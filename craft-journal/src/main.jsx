9edc30b1f32fe37040c7c465003ca58cda7a6ef7
klytne <tienganekelly@gmail.com>
2025-07-15 00:47:55 +0200

    Create vite template

---
+++ craft-journal/src/main.jsx
@@ -0,0 +1,12 @@
+// Entry point - react gets mounted to the DOM
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
